6c9749ddb1b08c1a1b71d28995a02c5d97d18435
Amal Jose <amal446446@gmail.com>
2025-12-30 15:35:02 +0530

    Implement error handling with Kafka producer

---
+++ scripts/errorHandler.js
@@ -0,0 +1,50 @@
+import { Kafka } from "kafkajs";
+import { randomUUID } from "crypto"
+
+const projectId = process.env.PROJECT_ID
+const deploymentId = process.env.DEPLOYMENT_ID
+const kafka = new Kafka({
+	clientId: `build-server-error-handler-${projectId}-${deploymentId}`,
+	brokers: ["pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092"],
+	ssl: true,
+	sasl: {
+		username: process.env.KAFKA_USERNAME,
+		password: process.env.KAFKA_PASSWORD,
+		mechanism: "plain"
+	},
+})
+
+const producer = kafka.producer()
+
+async function handleActionsFailure() {
+	if (!projectId || !deploymentId) {
+		console.error("Ids not received")
+		process.exit(1)
+	}
+	await producer.connect()
+	await producer.send({
+		topic: "deployment.updates", messages: [
+			{
+				key: "log", value: JSON.stringify(
+					{
+						eventId: randomUUID(),
+						eventType: 'DEPLOYMENT_UPDATES',
+						data: {
+							deploymentId: deploymentId,
+							projectId: projectId,
+							updateType: "ERROR",
+							updates: {
+								status: "FAILED",
+								error_message: "Failed to start build runner",
+							}
+						}
+					}
+				)
+			}
+		]
+	})
+}
+handleActionsFailure().catch((e) => {
+	console.log(e, "Failed to send erros")
+	process.exit(1)
+})
